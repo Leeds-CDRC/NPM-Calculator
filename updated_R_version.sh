2bbfd75e6e02e380083d93b076d80652de12571d
Maeve Murphy Quinlan <earmmu@leeds.ac.uk>
2024-10-17 14:14:35 +0100

    Update front page to swap location of columns

--- updated_R_version.sh
+++ updated_R_version.sh
@@ -1,3 +1,4 @@
 # R -e 'remotes::install_github("murphyqm/edits-nutrientprofiler", ref="146b787")'
 
-R -e 'remotes::install_github("murphyqm/edits-nutrientprofiler", ref="ab1144a")'
+# R -e 'remotes::install_github("murphyqm/edits-nutrientprofiler", ref="ab1144a")'
+R -e 'remotes::install_github("Leeds-CDRC/nutrientprofiler@v2.0.0")'
